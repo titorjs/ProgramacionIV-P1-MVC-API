2726e28460f9440a287b7001c0fe858ffa78ce16
Tito Jaramilo <tito.jaramillo@fisagrp.com>
2023-10-21 16:29:04 -0500

    Modelos terminados pre-migración

---
+++ ApiColegioPagos/Models/Estudiante.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApiColegioPagos.Models
+{
+    public class Estudiante
+    {
+        [Key]
+        //[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Est_id { get; set; }
+
+        [Required]
+        public int Est_cedula { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Est_nombre { get; set; }
+
+        [Required, MaxLength(150)]
+        public string Est_direccion { get; set; }
+
+        [Required]
+        public bool Est_activo { get; set; }
+
+        [Required, ForeignKey("Pen_id")]
+        public Pension Pen_id { get; set; }
+
+    }
+}
---
+++ ApiColegioPagos/Models/Pension.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiColegioPagos.Models
+{
+    public class Pension
+    {
+        [Key]
+        public int Pen_id { get; set; }
+
+        [Required, MaxLength(50)]
+        public string Pen_nombre { get; set; }
+
+        [Required]
+        [Range(0, 1000)]
+        [RegularExpression(@"^\d+(\.\d{2})?$")]
+        public float Pen_valor { get; set; }
+    }
+}
