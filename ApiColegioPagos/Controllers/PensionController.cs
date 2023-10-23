f5541991f0a9396e343ac163191f0d7d8ceb6180
titorjs <titoj740@gmail.com>
2023-10-23 08:48:57 -0500

    Avances Pension controller

---
+++ ApiColegioPagos/Controllers/PensionController.cs
@@ -0,0 +1,52 @@
+﻿using ApiColegioPagos.Data;
+using ApiColegioPagos.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiColegioPagos.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class PensionController : Controller
+	{
+		private readonly ApiColegioPagosDbContext _context;
+
+		public PensionController(ApiColegioPagosDbContext context)
+		{
+			_context = context;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> getAll()
+		{
+			List<Pension> pensiones = await _context.Pensiones.ToListAsync();
+			return Ok(pensiones);
+		}
+
+		[HttpGet("id/{id}")]
+		public async Task<IActionResult> getById(int id)
+		{
+			Pension pension = await _context.Pensiones.FirstOrDefaultAsync(x => x.Pen_id == id);
+			return Ok(pension);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> set([FromBody] Pension pension)
+		{
+			if(pension == null || pension.Pen_valor < 0 || pension.Pen_nombre == null)
+			{
+				return BadRequest("Los datos ingresados no son correctos");
+			}
+
+			await _context.Pensiones.AddAsync(pension);
+			await _context.SaveChangesAsync();
+
+			return Ok(pension);
+
+		}
+
+		//Debe considerar que no se puede eliminar si hay usuarios con esa pensión
+		//[HttpDelete]
+
+	}
+}
