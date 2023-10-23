a44b06359944a5e3bacb977d3ac413186bf91d24
unknown <julian.flores.armijos@udla.edu.ec>
2023-10-23 09:11:44 +0200

    Interfaz estudiante

---
+++ WebColegioPagos/Controllers/EstudianteController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebColegioPagos.Controllers
+{
+    public class EstudianteController : Controller
+    {
+        // GET: EstudianteController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: EstudianteController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: EstudianteController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EstudianteController/Create
+        
+
+        // GET: EstudianteController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: EstudianteController/Edit/5
+       
+
+        // GET: EstudianteController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+      
+    }
+}
