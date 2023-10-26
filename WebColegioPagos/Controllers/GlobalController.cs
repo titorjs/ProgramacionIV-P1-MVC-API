e6b4f60559cb5394c0c8c46c41b11ebfcfe07973
unknown <julian.flores.armijos@udla.edu.ec>
2023-10-26 21:43:32 +0200

    Interfaz web completa sin tanto diseño

---
+++ WebColegioPagos/Controllers/GlobalController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebColegioPagos.Controllers
+{
+    public class GlobalController : Controller
+    {
+        // GET: GlobalController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: GlobalController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: GlobalController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // GET: GlobalController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+
+        // GET: GlobalController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+    }
+}
